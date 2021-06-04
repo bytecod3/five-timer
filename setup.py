553fd6de0e3af3b67f301f6b8b15e24696915690
edwinmwiti <emwiti658@gmail.com>
2021-06-04 19:28:33 +0300

    added workflow files

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="five_timer",
-    version = "v_1.0",
+    version = "v1",
     description = "555-Timer calculator",
     author = "Edwin Mwiti",
     author_email = "emwiti658@gmail.com",
     url = "https://github.com/edwinmwiti/five-timer",
+    download_url = "https://github.com/edwinmwiti/five-timer/archive/refs/tags/v1.tar.gz",
     install_requires = ['Pillow'],
     packages = find_packages(),
-
     package_data = {
         "five_timer": ["resources/*"]
     },
